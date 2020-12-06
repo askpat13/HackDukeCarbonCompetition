30e6aed3d594730d867eca019cff1562733ebcf3
Patrick Wilson <patrick.m.wilson@duke.edu>
2020-12-05 23:50:02 -0500

    Updated settings page to have a form submission and submit button

--- carbon_competition/lib/Settings.dart
+++ carbon_competition/lib/Settings.dart
@@ -11,56 +11,89 @@ class Settings extends StatelessWidget {
            Text('Settings'),
         ),
       ),
-      body: Center(
+      body: CarbonForm(),
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+}
+
+class CarbonForm extends StatefulWidget {
+CarbonForm({Key key}) : super(key: key);
+
+@override
+_CarbonForm createState() => _CarbonForm();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _CarbonForm extends State<CarbonForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Center(
         child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'What do people call you?',
-              labelText: 'Name *',
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'What do people call you?',
+                labelText: 'Name *',
+              ),
+              onSaved: (String value) {
+                // This optional block of code can be used to run
+                // code when the user saves the form.
+              },
+              validator: (String value) {
+                return value.contains('@') ? 'Do not use the @ char.' : null;
+              },
             ),
-            onSaved: (String value) {
-              // This optional block of code can be used to run
-              // code when the user saves the form.
-            },
-            validator: (String value) {
-              return value.contains('@') ? 'Do not use the @ char.' : null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.circle),
-              hintText: 'What is the mileage per gallon of your car?',
-              labelText: 'Car MPG *',
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.circle),
+                hintText: 'What is the mileage per gallon of your car?',
+                labelText: 'Car MPG *',
+              ),
+              onSaved: (String value) {
+                // This optional block of code can be used to run
+                // code when the user saves the form.
+              },
+              validator: (String value) {
+                return value.contains('@') ? 'Do not use the @ char.' : null;
+              },
             ),
-            onSaved: (String value) {
-              // This optional block of code can be used to run
-              // code when the user saves the form.
-            },
-            validator: (String value) {
-              return value.contains('@') ? 'Do not use the @ char.' : null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.star),
-              hintText: 'Where is your location?',
-              labelText: 'ZIP CODE *',
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.star),
+                hintText: 'Where is your location?',
+                labelText: 'ZIP CODE *',
+              ),
+              onSaved: (String value) {
+                // This optional block of code can be used to run
+                // code when the user saves the form.
+              },
+              validator: (String value) {
+                return value.contains('@') ? 'Do not use the @ char.' : null;
+              },
             ),
-            onSaved: (String value) {
-              // This optional block of code can be used to run
-              // code when the user saves the form.
-            },
-            validator: (String value) {
-              return value.contains('@') ? 'Do not use the @ char.' : null;
-            },
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+              onPressed: () {
+              // Validate will return true if the form is valid, or false if
+              // the form is invalid.
+              if (_formKey.currentState.validate()) {
+              // Process data.
+              }
+              },
+                child: Text('Submit'),
+                ),
+              ),
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
