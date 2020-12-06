971dc7e8b5def4b6add7ab3469e168ffe23c2f5b
shufflesninja <isaact23@vt.edu>
2020-12-06 09:47:07 -0500

    Remove old database

--- database/dart/senddata.dart
+++
@@ -1,9 +0,0 @@
-
-Future<List> senddata() async {
-  final response = await http.post("http://raushanjha.in/insertdata.php", body: {
-    "name": user.text,
-    "email": pass.text,
-    "mobile":mobile.text,
-  });
-
-  var datauser = json.decode(response.body);
